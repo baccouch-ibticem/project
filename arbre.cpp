dd3dca34b9baa240b1b857b5464d25bde3173c65
Baccouch Ibticem <ibticembaccouch123@gmail.com>
2021-02-04 19:07:58 +0100

    master1

--- arbre.cpp
+++ arbre.cpp
@@ -75,7 +75,7 @@ void Supprimer(Noeud* noeud)
    Noeud* gauche = noeud->fils_gauche;
    Noeud* courant = arbre;
 
-   // Cas délicat : si on supprime la racine? V1
+   // Cas délicat : si on supprime la racine? V11
    if(noeud == arbre)
    {
       arbre = droite;
